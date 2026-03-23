435cce5c996923eb3417322e2cbc4f24ac3b1d6c
PinoNarciso <ebay@campolattaro.eu>
2026-03-23 16:30:38 +0100

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBSqc3y7CdmdsW86c5ZXPmnKOIdwcaJSTM",
+  authDomain: "giornaliero-tressetteaperdere.firebaseapp.com",
+  databaseURL: "https://giornaliero-tressetteaperdere-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "giornaliero-tressetteaperdere",
+  storageBucket: "giornaliero-tressetteaperdere.firebasestorage.app",
+  messagingSenderId: "630006722305",
+  appId: "1:630006722305:web:8c09a17747edaab5fc7500"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = firebase.database();
